098bc814c454c3e05f1a4b4e196fa376c6d97d3c
OnoprienkoArtem <o.artem.e@gmail.com>
2020-10-20 22:41:23 +0300

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,8 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { 
-                    from: path.resolve(__dirname, 'src/favicon.ico'), 
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
                     to: path.resolve(__dirname, 'dist'),
                 },
             ],
@@ -36,5 +36,28 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'bundle.[hash].css'
         }),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
+        ],
+    },
+
 }
