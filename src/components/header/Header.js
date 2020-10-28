fc286ca38be1be1b9ca3b9f623e3a0f55b228e9a
OnoprienkoArtem <o.artem.e@gmail.com>
2020-10-28 23:19:20 +0200

    fill layout in appropriate classes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,17 @@ export class Header extends ExcelComponent {
   static className = 'excel__header';
 
   toHTML() {
-    return '<h1>Header</h1>';
+    return `    
+      <input class="input" type="text" value="New table" />
+
+      <div>
+          <div class="button">
+              <i class="material-icons">delete</i>
+          </div>
+          <div class="button">
+              <i class="material-icons">exit_to_app</i>
+          </div>
+      </div>
+    `;
   }
 }
