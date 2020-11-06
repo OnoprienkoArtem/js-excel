613d87aeaf759931f94b82f370423f2b652a2f37
OnoprienkoArtem <o.artem.e@gmail.com>
2020-11-06 23:16:27 +0200

    work with geting coordinats

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
+import { $ } from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -17,6 +18,10 @@ export class Table extends ExcelComponent {
   onMousedown(event) {
     if (event.target.dataset.resize) {
       console.log(event.target.dataset.resize);
+      const $resizer = $(event.target);
+      const $parent = $resizer.closest('[data-type="resizer"]');
+
+      console.log($parent);
     }
   }
 }
