56c33ac2354cd3994dee2b8a2684b8ef2c3cbf02
OnoprienkoArtem <o.artem.e@gmail.com>
2020-11-10 22:44:28 +0200

    work with moving resizer

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -22,22 +22,40 @@ export class Table extends ExcelComponent {
       const coords = $parent.getCoords();
       const sells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
       const type = $resizer.data.resize;
+      let value;
+
+      $resizer.css({ 
+        opacity: 1,
+        bottom: '-5000px',
+      });
 
       document.onmousemove = e => {
         if (type === 'col') {
           const delta = e.pageX - coords.right;
-          const value = coords.width + delta;
+          value = coords.width + delta;
+          $resizer.css({ right: -delta + 'px' });
+
           $parent.css({ width: value + 'px' });
           sells.forEach(el => el.style.width = value + 'px');
         } else {
-          const delta = e.pageY - event.pageY;
-          const value = coords.height + delta;
-          $parent.css({ height: value + 'px' });
+          // const delta = e.pageY - event.pageY;
+          // const value = coords.height + delta;
+          // $parent.css({ height: value + 'px' });
         }
       };
 
       document.onmouseup = () => {
         document.onmousemove = null;
+        document.onmouseup = null;
+
+        $parent.css({ width: value + 'px' });
+        sells.forEach(el => el.style.width = value + 'px');
+
+        $resizer.css({ 
+          opacity: 0,
+          bottom: 0,
+          right: 0,
+        });
       };
     }
   }
