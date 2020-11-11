93c39a95b6756d4069eb7acddcfbb29b2d48a45e
OnoprienkoArtem <o.artem.e@gmail.com>
2020-11-11 23:12:45 +0200

    move logic if we should aplly resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
 import { resizeHandler } from './table.resize';
+import { shouldResize } from './table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -16,7 +17,7 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
+    if (shouldResize(event)) {
       resizeHandler(this.$root, event);
     }
   }
