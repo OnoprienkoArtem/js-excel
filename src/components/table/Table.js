2cce3834eb937483f8a6e7d27638e771c15f7f9c
OnoprienkoArtem <o.artem.e@gmail.com>
2020-11-14 21:30:12 +0200

    refactoring

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,10 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
 import { resizeHandler } from './table.resize';
-import { shouldResize, isCell } from './table.functions';
+import { shouldResize, isCell, matrix } from './table.functions';
 import { TableSelection } from './TableSelection';
 import { $ } from '@core/dom';
+
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -35,32 +36,11 @@ export class Table extends ExcelComponent {
     } else if (isCell(event)) {
       const $target = $(event.target);
       if (event.shiftKey) {
-        const target = $target.id(true);
-        const current = this.selection.current.id(true);
-
-        const cols = range(current.col, target.col);
-        const rows = range(current.row, target.row);
-
-        const ids = cols.reduce((acc, col) => {
-          rows.forEach(row => acc.push(`${row}:${col}`));
-          return acc;
-        }, []);
-
-        const $cells = ids.map(id => this.$root.find(`[data-id="${id}"]`));
+        const $cells = matrix($target, this.selection.current).map(id => this.$root.find(`[data-id="${id}"]`));
         this.selection.selectGroup($cells);
       } else {
         this.selection.select($target);
       }
     }
   }
-}
-
-function range(start, end) {
-  if (start > end) {
-    [end, start] = [start, end];
-  }
-
-  return new Array(end - start + 1)
-      .fill('')
-      .map((_, index) => start + index);
 }
