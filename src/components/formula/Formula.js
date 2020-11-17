74d638b9d1e5c9558bf9ed69ce752fdd3d4b6d24
OnoprienkoArtem <o.artem.e@gmail.com>
2020-11-17 22:54:19 +0200

    create emmiter

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -6,7 +6,8 @@ export class Formula extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click'],
+      listeners: ['input'],
+      ...options,
     });
   }
 
@@ -18,10 +19,7 @@ export class Formula extends ExcelComponent {
   }
 
   onInput(event) {
-    console.log('Formula: onInput', event);
-  }
-
-  onClick(event) {
-    console.log('Formula: onClick', event);
+    const text = event.target.textContent.trim();
+    this.emitter.emit('it is working', text);
   }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -9,9 +9,11 @@ import { $ } from '@core/dom';
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
+      name: 'Table',
       listeners: ['mousedown', 'keydown'],
+      ...options,
     });
   }
 
@@ -28,6 +30,10 @@ export class Table extends ExcelComponent {
 
     const $cell = this.$root.find('[data-id="0:0"]');
     this.selection.select($cell);
+
+    this.emitter.subscribe('it is working', text => {
+      this.selection.current.text(text);
+    });
   }
 
   onMousedown(event) {
