9ef3f053aa985ffca35e2677fc3132ac2dfaa741
OnoprienkoArtem <o.artem.e@gmail.com>
2020-12-04 23:36:28 +0200

    optimize subscribe logic, implement store subscriber class

--- src/core/StoreSubscriber.js
+++ src/core/StoreSubscriber.js
@@ -1,13 +1,16 @@
 export class StoreSubscriber {
   constructor(store) {
     this.store = store;
+    this.sub = null;
   }
 
-  subscribeComponents() {
+  subscribeComponents(components) {
+    this.sub = this.store.subscribe(state => {
 
+    });
   }
 
   unsubscribeFromStore() {
-
+    this.sub.unsubscribe();
   }
 }
