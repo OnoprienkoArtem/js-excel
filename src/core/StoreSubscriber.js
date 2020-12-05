75e4852a0941afc58e1e616fa07ea63e6abadad3
OnoprienkoArtem <o.artem.e@gmail.com>
2020-12-05 22:32:32 +0200

    optimize subscribe logic

--- src/core/StoreSubscriber.js
+++ src/core/StoreSubscriber.js
@@ -2,11 +2,17 @@ export class StoreSubscriber {
   constructor(store) {
     this.store = store;
     this.sub = null;
+    this.prevState = {};
   }
 
   subscribeComponents(components) {
+    this.prevState = this.store.getState();
     this.sub = this.store.subscribe(state => {
+      Object.keys(state).forEach(key => {
+        if (!isEqual(this.prevState[key], state[key])) {
 
+        }
+      })
     });
   }
 
