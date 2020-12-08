b6879bc18eac75daddc7321197a6dfb0002eb00f
OnoprienkoArtem <o.artem.e@gmail.com>
2020-12-08 22:42:50 +0200

    process click event on toolbar buttons

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import {createToolbar} from './toolbar.template';
+import {$} from "@core/dom";
 
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar';
@@ -7,12 +8,20 @@ export class Toolbar extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
-      listeners: [],
+      listeners: ['click'],
       ...options,
     });
   }
 
   toHTML() {
     return createToolbar();
+  }
+
+  onClick(event) {
+    const $target = $(event.target);
+
+    if ($target.data.type === 'button') {
+
+    }
   }
 }
--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -1,7 +1,20 @@
 function toButton(button) {
+  const meta = `
+    data-type="button"
+    data-value='${JSON.stringify(button.value)}'
+  `;
+
   return `
-    <div class="button ${button.active ? 'active' : ''}">
-        <i class="material-icons">${button.icon}</i>
+    <div 
+      class="button ${button.active ? 'active' : ''}"
+      ${meta}
+    >
+      <i 
+        class="material-icons" 
+        ${meta}
+      >
+        ${button.icon}
+      </i>
     </div>
   `;
 }
@@ -11,26 +24,44 @@ export function createToolbar() {
     {
       icon: 'format_align_left',
       active: false,
+      value: {
+        textAlign: 'left',
+      },
     },
     {
       icon: 'format_align_center',
-      active: true,
+      active: false,
+      value: {
+        textAlign: 'center',
+      },
     },
     {
       icon: 'format_align_right',
       active: false,
+      value: {
+        textAlign: 'right',
+      },
     },
     {
       icon: 'format_bold',
-      active: true,
+      active: false,
+      value: {
+        fontWeight: 'bold',
+      },
     },
     {
       icon: 'format_italic',
       active: false,
+      value: {
+        fontStyle: 'italic',
+      },
     },
     {
       icon: 'format_underlined',
       active: false,
+      value: {
+        textDecoration: 'underlined',
+      },
     },
   ];
 
