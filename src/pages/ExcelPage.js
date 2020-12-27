a39bd5696c60167147ccb6ad2dde9c9bc955446c
OnoprienkoArtem <o.artem.e@gmail.com>
2020-12-27 22:47:30 +0200

    redid initial state

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,6 +1,6 @@
 import {Page} from '@core/Page';
 import {createStore} from '@core/createStore';
-import {initialState} from '@/redux/initialState';
+import {initialState, normalizeInitialState} from '@/redux/initialState';
 import {rootReducer} from '@/redux/rootReducer';
 import {debounce, storage} from '@core/utils';
 import {Excel} from '@/components/excel/Excel';
@@ -9,12 +9,17 @@ import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 
+function storageName(param) {
+  return 'excel:' + param;
+}
+
 export class ExcelPage extends Page {
   getRoot() {
-    const store = createStore(rootReducer, initialState);
+    const state = storage(storageName(this.params));
+    const store = createStore(rootReducer, normalizeInitialState(state));
 
     const stateListener = debounce(state => {
-      storage('excel-state', state);
+      storage(storageName(this.params), state);
     }, 300);
 
     store.subscribe(stateListener);
