dcf561ca4685abff8ffef4ecbfc48584ba9f3908
OnoprienkoArtem <o.artem.e@gmail.com>
2021-01-02 23:40:00 +0200

    testing create store

--- src/core/store/createStore.spec.js
+++ src/core/store/createStore.spec.js
@@ -1,3 +1,32 @@
-describe('', () => {
+import { createStore } from './createStore';
 
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'ADD') {
+    return {...state, count: state.count + 1};
+  }
+
+  return state;
+}
+
+describe('createStore:', () => {
+  let store;
+
+  beforeEach(() => {
+    store = createStore(reducer);
+  });
+
+  test('should return store object', () => {
+    expect(store).toBeDefined();
+    expect(store.dispatch).toBeDefined();
+    expect(store.subscribe).toBeDefined();
+    expect(store.getState).not.toBeUndefined();
+  });
+
+  test('should return object as a state', () => {
+    expect(store.getState()).toBeInstanceOf(Object);
+  });
 });
