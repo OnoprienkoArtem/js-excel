bda6dfb2e170c5c89e9c3a62e0d841672e5297cf
OnoprienkoArtem <o.artem.e@gmail.com>
2021-01-03 22:06:19 +0200

    finish testing create store

--- src/core/store/createStore.spec.js
+++ src/core/store/createStore.spec.js
@@ -14,9 +14,11 @@ const reducer = (state = initialState, action) => {
 
 describe('createStore:', () => {
   let store;
+  let handler;
 
   beforeEach(() => {
     store = createStore(reducer, initialState);
+    handler = jest.fn();
   });
 
   test('should return store object', () => {
@@ -34,8 +36,43 @@ describe('createStore:', () => {
     expect(store.getState()).toEqual(initialState);
   });
 
+  test('should change state if action exists', () => {
+    store.dispatch({type: 'ADD'});
+    expect(store.getState().count).toBe(1);
+  });
+
   test('should not change state if action don\'t exists', () => {
     store.dispatch({type: 'NOT_EXISTING_ACTION'});
     expect(store.getState().count).toBe(0);
+  });
+
+  test('should call subscriber function', () => {
+    store.subscribe(handler);
+    store.dispatch({type: 'ADD'});
+
+    expect(handler).toHaveBeenCalled();
+    expect(handler).toHaveBeenCalledWith(store.getState());
+  });
+
+  test('should not call subscriber if unsubscribe', () => {
+    const sub = store.subscribe(handler);
+
+    sub.unsubscribe();
+
+    store.dispatch({type: 'ADD'});
+
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  test('should dispatch in async way', () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        store.dispatch({type: 'ADD'});
+      }, 500);
+      setTimeout(() => {
+        expect(store.getState().count).toBe(1);
+        resolve();
+      }, 1000);
+    });
   });
 });
